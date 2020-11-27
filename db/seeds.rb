7703085e97faccd743fd27847584c14d499c8c93
kojiro_abeki <kojiroki@hotmail.com>
2020-11-27 17:17:51 -0300

    Rails Simple Airbnb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the db..."
+
+Flat.destroy_all
+
+puts "db is clean"
+
+puts "creating restaurants..."
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Flat Versailles',
+  address: 'Av Nestor Clifton 540 Curitiba',
+  description: 'Era um Flat que tinha em Curitiba bom e relativamente barato e, principalmente, pertinho do Serpro',
+  price_per_night: 35,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Night Espace Mind Flat France',
+  address: 'Clifford goes to Space 99 Paris',
+  description: 'A new garden steamer en San Francisco goes to two double bedrooms, open plan living area, no kitchen and a beautiful conservatory',
+  price_per_night: 45,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'This will help us to get started',
+  address: 'Avenida Rua da Frente, 100, Florida SE',
+  description: 'O mais perto da Rua da Frente e tem two double bedrooms, open plan living area, com a maior kitchen do mundo a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 5
+)
+
+puts "finished"
